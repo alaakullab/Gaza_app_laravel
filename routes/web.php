7703790b5cf6dcc2600b783e0655421ee42ee23e
Alaa Kullab <alaa2kullab@gmail.com>
2018-12-11 09:52:07 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+	return '<h3>Gaza App Page</h3>';
+   // return view('welcome');
+});
+
+Route::get('/about', function () {
+	$response_arr = [];
+	$response_arr['author'] = 'BP';
+	$response_arr['version'] = '0.1.1';
+	return $response_arr;
+});
+
+Route::get('/home', function () {
+	$data = [];
+	$data['version'] = '0.1.1';
+	return view('welcome', $data);
+});
