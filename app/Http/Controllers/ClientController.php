e415349705e36fb99ff8fa1381dbe7ab5a5e6a0a
Alaa Kullab <alaa2kullab@gmail.com>
2018-12-12 06:49:32 +0200

    edit clinte controller

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class ClientController extends Controller
 {
     //
+        public function __construct( Title $titles )
+    {
+    	$this->titles = $titles->all();
+    }
+
+    public function di()
+    {
+    	dd($this->titles);
+    }
 }
