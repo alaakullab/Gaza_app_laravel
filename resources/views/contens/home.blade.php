029fae8bf4c728c580292fb2d7129e9bf94a5ec6
Alaa Kullab <alaa2kullab@gmail.com>
2018-12-14 16:42:54 +0200

    Create a template and extended views - Done

---
+++ resources/views/contens/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="row">
+      <div class="medium-6 columns">
+        <h4>Gaza Hotel App</h4>
+        <img class="thumbnail" src="images/attractions.jpg">
+      </div>
+      <div class="medium-6 large-5 columns">
+        <p>The original Gaza Strip perseveres after 50 years in the heart Gaza. The West End neighborhood has something for everyone—from theater to dining to historic sights.</p>
+        <p>And the not-to-miss Roof Cafe is a great place for travelers and locals to engage over drinks, food, and good conversation.</p>
+        <p>To learn more about the Gaza Hotel, browse our website and download our handy information sheet.</p>
+      </div>
+    </div>
+@endsection
