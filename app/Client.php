5ad2ee5f84adeccf932c0d7e54a45b1ac948ae9c
Alaa Kullab <alaa2kullab@gmail.com>
2018-12-18 14:34:35 +0200

    Relations in Eloquent

--- app/Client.php
+++ app/Client.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     //
+
+    public function reservations()
+    {
+    	return $this->hasMany('App\Reservation');
+    }
 }
--- app/Reservation.php
+++ app/Reservation.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     //
+    public function client()
+    {
+    	return $this->belongsTo('App\Client', 'client_id', 'id');
+    }
 }
