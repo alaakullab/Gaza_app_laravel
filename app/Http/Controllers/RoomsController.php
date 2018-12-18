e7d00dafdc854515426d207baab33dfb3b9801f0
Alaa Kullab <alaa2kullab@gmail.com>
2018-12-18 19:40:34 +0200

    Select data with raw queries

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -8,8 +8,19 @@ class RoomsController extends Controller
 {
     //
 
-	public function checkAvailableRooms()
+	public function checkAvailableRooms($client_id, Request $request)
 	{
+			$dateForm = $request->input('dateForm');
+			$dateTo = $request->input('dateTo');
+			$client = New Client();
+			$room = New Room();
+
+			$data = [];
+			$data['dateForm'] = $dateForm;
+			$data['dateTo'] = $dateTo;
+			$data['room'] = $room->getAvailableRooms($dateForm, $dateTo);
+			$data['client'] = $client->find($client_id);
+
 		  	return view('rooms/checkAvailableRooms');
 	}
 }
--- app/room.php
+++ app/room.php
@@ -3,8 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Room extends Model
 {
     //
+
+    public function getAvailabelrooms($start_date, $end_date)
+    {
+    	$available_rooms = DB::table('rooms as r')
+    								->select('r.id', 'r.name')
+    								->whereRow("
+    									r.id NOT IN (
+    										select b.room_id FROM reservations b
+    										WHERE NOT(
+    										b.date_out < '{$start_date}' OR
+    										b.date_in > '{$end_date}'
+    									) 
+    									")
+    								->orderBy('r.id')
+    								->get()
+    	;
+    	return $available_rooms;
+    }
 }
